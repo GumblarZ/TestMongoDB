e9ab7254ddbf660fcb0d4ee6f7ff8986a72d99d0
GumblarZ <jc0d3001@outlook.com>
2022-02-07 08:50:38 -0300

    limpeza e identacao

--- src/database.js
+++ src/database.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
-
+const link = "mongodb+srv://gumblarz:$darklaw24@cluster0.5wbt1.mongodb.net/genshinData?retryWrites=true&w=majority";
 function connectToDatabase(){
-    mongoose.connect("mongodb+srv://gumblarz:$darklaw24@cluster0.5wbt1.mongodb.net/genshinData?retryWrites=true&w=majority"
-    ,
-    {
+    mongoose.connect(link,{
         useNewUrlParser: true,
         useUnifiedTopology:true
     });
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const routes = express.Router();
 
-const VideoController = require("./controllers/videoController");
 const CharactereController = require("./controllers/charactereController");
-
-routes.get("/videos", VideoController.index);
-routes.post("/videos", VideoController.store);
 
 routes.get("/characteres", CharactereController.index);
 routes.post("/characteres", CharactereController.store);
